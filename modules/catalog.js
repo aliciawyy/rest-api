cb97af8784a539f06253d858422364ac2b9f8d17
aliciawyy <rainingilove@gmail.com>
2018-07-26 11:16:21 +0200

    implement api catalog

--- modules/catalog.js
+++ modules/catalog.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
+const path = require('path');
 
 function readCatalogSync () {
-  let file = '../data/catalog.json';
+  let file = path.join(__dirname, '../data/catalog.json');
   if (fs.existsSync(file)) {
     let content = fs.readFileSync(file);
     return JSON.parse(content);
--- routes/catalog.js
+++ routes/catalog.js
@@ -1,8 +1,35 @@
 const express = require('express');
+const url = require('url');
+const catalog = require('../modules/catalog');
 const router = express.Router();
 
-router.get('/', function (req, res, next) {
-  res.send('respond with a catalog');
+function findIndex (index, res) {
+  let indexInfo = catalog.findIndex(index);
+  if (indexInfo) {
+    res.json(indexInfo);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not found');
+  }
+}
+
+/*
+* e.g. /catalog or /catalog?index=CRC3
+* */
+router.get('/', (req, res) => {
+  let params = url.parse(req.url, true).query;
+  if (Object.keys(params).length === 0) {
+    res.json(catalog.readCatalogSync());
+  } else {
+    findIndex(params.index, res);
+  }
+});
+
+/*
+* e.g. /catalog/CRC3
+* */
+router.get('/:index', (req, res) => {
+  findIndex(req.params.index, res);
 });
 
 module.exports = router;
